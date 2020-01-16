97aee3acf357bb9b826e29fd727c84760e6ed10c
Bruno FACHE <b.fache@ludus-academie.com>
2020-01-16 17:03:46 +0100

    Premiers test kinect

---
+++ Assets/Scripts/Mouvement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+
+public class Mouvement : MonoBehaviour {
+
+    public GameObject BodySrcManager;
+    public JointType TrackedJoint;
+    private Kinect bodyManager;
+    private Body[] bodies;
+    public float multiplier = 1;
+
+    // Use this for initialization
+    void Start () {
+        bodyManager = BodySrcManager.GetComponent<Kinect>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        bodies = bodyManager.GetData();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+               
+
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PositionDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+
+public class PositionDetection : MonoBehaviour {
+
+    public GameObject BodySrcManager;
+    public JointType TrackedHead;
+    public JointType TrackedHandLeft;
+    public JointType TrackedHandRight;
+    private Kinect bodyManager;
+    private Body[] bodies;
+    public float multiplier = 1;
+
+    // Use this for initialization
+    void Start () {
+        bodyManager = BodySrcManager.GetComponent<Kinect>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bodies = bodyManager.GetData();
+    }
+
+   public CameraSpacePoint PositionHead()
+    {
+        CameraSpacePoint pos;
+        pos.X = 1;
+        foreach (var body in bodies)
+        {
+            if (body.IsTracked)
+            {
+                pos = body.Joints[TrackedHead].Position;
+                
+            }
+        }
+        return pos;
+    }
+}
