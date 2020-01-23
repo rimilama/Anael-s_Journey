7623b83c3cd39e178f3028245977262c48c9fcd5
Bruno FACHE <b.fache@ludus-academie.com>
2020-01-23 16:58:57 +0100

    Cite avec la faute \o/

--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -1,39 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Windows.Kinect;
 
 
 public class Mouvement : MonoBehaviour {
 
-    public GameObject BodySrcManager;
-    public JointType TrackedJoint;
-    private Kinect bodyManager;
-    private Body[] bodies;
+    public PositionDetection PositionDetect;
+    public Text Validation;
+    //CameraSpacePoint posNeck;
+    //CameraSpacePoint posHead;
+    //CameraSpacePoint posMid;
+    CameraSpacePoint posHandLeft;
+    CameraSpacePoint posElbowLeft;
     public float multiplier = 1;
+    float tempsStart = 0.0f;
+    bool positionStart = false;
+    bool positionEnd = false;
 
     // Use this for initialization
     void Start () {
-        bodyManager = BodySrcManager.GetComponent<Kinect>();
-
+      
     }
 
     // Update is called once per frame
     void Update () {
-        bodies = bodyManager.GetData();
-
-        foreach (var body in bodies)
+        if(this.name == "elbowleft")
         {
-            if (body == null)
-            {
-                continue;
-            }
-            if (body.IsTracked)
-            {
-               
+            posElbowLeft = PositionDetect.PositionElbowLeft();
+            this.transform.position = new Vector3(posElbowLeft.X * multiplier, posElbowLeft.Y * multiplier);
+        }
+        else if(this.name == "handleft")
+        {
+            posHandLeft = PositionDetect.PositionHandLeft();
+            this.transform.position = new Vector3(posHandLeft.X * multiplier, posHandLeft.Y * multiplier);
 
-            }
+        }
+        /*else if(this.name == "mid")
+        {
+            posMid = PositionDetect.PositionMid();
+            this.transform.position = new Vector3(posMid.X * multiplier, posMid.Y * multiplier);
         }
 
+        Debug.Log("cou : " + posHead.Y * multiplier);
+        Debug.Log("main : " + posHandLeft.Y * multiplier);
+        */
+
+        if (posHandLeft.X < posElbowLeft.X && posHandLeft.Y > posElbowLeft.Y && positionEnd == false)
+        {
+            tempsStart = Time.time;
+            positionStart = true;
+            Validation.text = "HAUT";
+        }
+        if(posHandLeft.X > posElbowLeft.X && posHandLeft.Y > posElbowLeft.Y && positionStart == true && Time.time - tempsStart > 2)
+        {
+            positionStart = false;
+            Validation.text = "RESET";
+        }
+        if(posHandLeft.X > posElbowLeft.X && posHandLeft.Y < posElbowLeft.Y && positionStart == true && Time.time-tempsStart < 2)
+        {
+            positionStart = false;
+            positionEnd = true;
+        }
+        if(positionEnd == true)
+        {
+            Validation.text = "Bienvenue !";
+            positionEnd = false;
+        }
+
+        //Debug.Log("END : " + positionEnd);
+        //Debug.Log("START : " + positionStart);
     }
 }
--- Assets/Scripts/PositionDetection.cs
+++ Assets/Scripts/PositionDetection.cs
@@ -8,8 +8,11 @@ public class PositionDetection : MonoBehaviour {
 
     public GameObject BodySrcManager;
     public JointType TrackedHead;
+    public JointType TrackedNeck;
     public JointType TrackedHandLeft;
+    public JointType TrackedElbowLeft;
     public JointType TrackedHandRight;
+    public JointType TrackedMid;
     private Kinect bodyManager;
     private Body[] bodies;
     public float multiplier = 1;
@@ -26,16 +29,133 @@ public class PositionDetection : MonoBehaviour {
 
    public CameraSpacePoint PositionHead()
     {
-        CameraSpacePoint pos;
-        pos.X = 1;
+        CameraSpacePoint pos = new CameraSpacePoint();
+
         foreach (var body in bodies)
         {
-            if (body.IsTracked)
+            if (body == null)
+            {
+                pos.X = 0.0f;
+                pos.Y = 0.0f;
+                pos.Z = 0.0f;
+            }
+            else if (body.IsTracked)
             {
                 pos = body.Joints[TrackedHead].Position;
-                
+                return pos;
             }
         }
+
+        return pos;
+    }
+
+    public CameraSpacePoint PositionNeck()
+    {
+        CameraSpacePoint pos = new CameraSpacePoint();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                pos.X = 0.0f;
+                pos.Y = 0.0f;
+                pos.Z = 0.0f;
+            }
+            else if (body.IsTracked)
+            {
+                pos = body.Joints[TrackedNeck].Position;
+                return pos;
+            }
+        }
+
+        return pos;
+    }
+
+    public CameraSpacePoint PositionHandRight()
+    {
+        CameraSpacePoint pos = new CameraSpacePoint();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                pos.X = 0.0f;
+                pos.Y = 0.0f;
+                pos.Z = 0.0f;
+            }
+            else if (body.IsTracked)
+            {
+                pos = body.Joints[TrackedHandRight].Position;
+                return pos;
+            }
+        }
+
+        return pos;
+    }
+
+    public CameraSpacePoint PositionHandLeft()
+    {
+        CameraSpacePoint pos = new CameraSpacePoint();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                pos.X = 0.0f;
+                pos.Y = 0.0f;
+                pos.Z = 0.0f;
+            }
+            else if (body.IsTracked)
+            {
+                pos = body.Joints[TrackedHandLeft].Position;
+                return pos;
+            }
+        }
+
+        return pos;
+    }
+
+    public CameraSpacePoint PositionElbowLeft()
+    {
+        CameraSpacePoint pos = new CameraSpacePoint();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                pos.X = 0.0f;
+                pos.Y = 0.0f;
+                pos.Z = 0.0f;
+            }
+            else if (body.IsTracked)
+            {
+                pos = body.Joints[TrackedElbowLeft].Position;
+                return pos;
+            }
+        }
+
+        return pos;
+    }
+
+    public CameraSpacePoint PositionMid()
+    {
+        CameraSpacePoint pos = new CameraSpacePoint();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                pos.X = 0.0f;
+                pos.Y = 0.0f;
+                pos.Z = 0.0f;
+            }
+            else if (body.IsTracked)
+            {
+                pos = body.Joints[TrackedMid].Position;
+                return pos;
+            }
+        }
+
         return pos;
     }
 }
